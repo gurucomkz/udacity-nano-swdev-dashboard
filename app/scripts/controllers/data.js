217942c42c1b7e41834d3f01ff0db1b0854da9f1
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-08-22 14:44:22 +0700

    feature: data table w/ sorting/filtering/paging

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -13,9 +13,100 @@ angular.module('dashboardApp')
     'myService',
     '$rootScope',
 function ($scope, myService, $rootScope) {
-    $scope.issues = myService.allIssues;
+    var issues = myService.allIssues,
+        filtered = [];
+
+    $scope.filterText = '';
+
+    $scope.sortingField = 'submissionTimestamp';
+    $scope.sortingDirection = 'asc';
+
+    $scope.displayNumber = 10;
+    $scope.displayPage = 1;
+    $scope.displayPagerPages = [];
+    $scope.displayPageCount = 0;
+    $scope.setPage = function(toPage){
+        $scope.displayPage = toPage;
+        fixDisplay();
+    }
+
+    $scope.displayIssues = [];
+    $scope.toggleSorting = function(newKey){
+        if(newKey != $scope.sortingField){
+            $scope.sortingDirection = 'asc';
+            $scope.sortingField = newKey;
+        }else{
+            $scope.sortingDirection = ($scope.sortingDirection == 'asc') ? 'desc' : 'asc';
+        }
+        performSorting();
+    };
+
+    function performSorting(){
+        var sortAsc = ($scope.sortingDirection == 'asc');
+        filtered.sort(function(a,b){
+            var av = a[$scope.sortingField],
+                bv = b[$scope.sortingField];
+            if(av == bv){ return 0; }
+            if(av > bv){ return sortAsc ? 1 : -1; }
+            return sortAsc ? -1 : 1;
+        });
+        fixDisplay();
+    }
+
+    $scope.$watch('displayNumber', fixDisplay);
+    $scope.$watch('filterText', fixFilter);
+
+    function fixFilter(){
+        if(!$scope.filterText.length){
+            filtered = issues;
+        }else{
+            filtered = issues.filter(function(entry){
+                for(var k in entry){
+                    if((''+entry[k]).indexOf($scope.filterText)>=0) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+        }
+
+        $scope.displayPageCount = Math.ceil(filtered.length / $scope.displayNumber);
+        performSorting();
+    }
+
+    function fixDisplay(){
+        $scope.displayStart = ($scope.displayPage - 1)  * $scope.displayNumber;
+        $scope.displayEnd = Math.min($scope.displayStart + $scope.displayNumber, filtered.length-1);
+
+        $scope.filteredTotal = filtered.length;
+
+        var candidates = filtered.slice($scope.displayStart, $scope.displayEnd);
+        $scope.displayIssues.length = 0;
+        for(var x in candidates){
+            $scope.displayIssues.push(candidates[x]);
+        }
+    }
+
 
     $rootScope.$on('issuesUpdated', function(){
-        $scope.issues = myService.allIssues;
+        issues = myService.allIssues;
+        fixFilter();
     });
-}]);
+}])
+
+.directive('dtSorting',[
+
+function(){
+    return {
+        restrict: 'A',
+        scope: {
+            sortKey: '@dtSorting',
+            sortField: '=',
+            sortDirection: '='
+        },
+        replace: true,
+        transclude: true,
+        template: '<th ng-click="$parent.toggleSorting(sortKey)" ng-transclude ng-class="{ \'sorting\': sortKey!=sortField, \'sorting_asc\' : sortKey==sortField && sortDirection == \'asc\', \'sorting_desc\' : sortKey==sortField && sortDirection == \'desc\' }">{{title}}</th>'
+    };
+}
+])
