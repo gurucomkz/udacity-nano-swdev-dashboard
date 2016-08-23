ca3e71423aa2287f15205a10246bbad250631507
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-08-23 19:27:04 +0700

    fix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,7 +232,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '!<%= yeoman.dist %>/images/ignore',
+          '!<%= yeoman.dist %>/images/ignore/**',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
